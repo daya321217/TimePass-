ae77f5d738381e10ddcb35e8527983b04f71f65e
DAYASHREE S <dayashree.s@s.amity.edu>
2025-06-01 22:13:09 +0530

    Update Timed_math_challenge.py

--- Timed_math_challenge.py
+++ Timed_math_challenge.py
@@ -1,8 +1,10 @@
 import random
 
+#Constants that dont change in CAPS
 OPERATOR =["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -10,7 +12,12 @@ def generate_problem():
     operator = random.choice(OPERATOR)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr , answer
 
-generate_problem()
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ":" + expr + " = ") 
+        if guess == str(answer):
+            break
